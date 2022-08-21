0b52a3927edd3c31ce303837e0613b2bfd3ef71d
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-21 13:10:40 +0300

    add last commit in phpstorm

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
