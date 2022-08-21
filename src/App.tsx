8c5772a39d0aa64ca7c0d9427684a7ebadef731f
stelsp <stelspeh@gmail.com>
2022-08-21 12:16:13 +0300

    feat: i @types/react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/"/>
+        <Route path="/" />
       </Routes>
     </>
   );
