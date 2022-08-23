e93fc74a872cd0a213f521c3f68dfc6e56ff9fbc
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-23 14:38:05 +0300

    feat: add content in EducationalVideos.tsx

--- src/components/EducationalVideos/index.tsx
+++ src/components/EducationalVideos/index.tsx
@@ -3,7 +3,55 @@ import React, { FC } from 'react';
 import style from './style.module.scss';
 
 const EducationalVideos: FC = () => {
-  return <div className={style.educationalVideos}>EducationalVideos</div>;
+  return (
+    <section
+      className={style.educationalVideos}
+      id="educationalVideos"
+      aria-label="Обучающие видео"
+    >
+      <div className={style.container}>
+        <h2 className={style.title}>Видео</h2>
+        <div className={style.videos}>
+          <iframe
+            width="530"
+            height="285"
+            src="https://www.youtube.com/embed/cGbEUDeRtzE"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <iframe
+            width="530"
+            height="285"
+            src="https://www.youtube.com/embed/3GDWXTBJpfc"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <iframe
+            width="530"
+            height="285"
+            src="https://www.youtube.com/embed/bglVOxMaM70"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <iframe
+            width="530"
+            height="285"
+            src="https://www.youtube.com/embed/3im6wLx7b24"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default EducationalVideos;
