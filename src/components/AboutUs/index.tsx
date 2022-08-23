a679b850159671f8f53382656dee2dc9d2c299e9
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-23 14:37:36 +0300

    feat: add content in AboutUs.tsx

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -3,7 +3,26 @@ import React, { FC } from 'react';
 import style from './style.module.scss';
 
 const AboutUs: FC = () => {
-  return <div className={style.aboutUs}>AboutUs</div>;
+  return (
+    <section className={style.aboutUs} id="aboutUs" aria-label="О нас">
+      <div className={style.container}>
+        <h2 className={style.title}>О нас</h2>
+        <p className={style.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate quibusdam
+          doloremque reprehenderit, modi praesentium obcaecati rem totam aut veritatis similique,
+          debitis a dignissimos perspiciatis quam! Suscipit inventore itaque consequatur. Nesciunt
+          accusantium nulla, repellendus explicabo laudantium perspiciatis, consectetur libero,
+          mollitia consequatur expedita officia sed nostrum sit molestiae nobis. Eos ducimus nostrum
+          quaerat ea dolorem aut esse asperiores eaque. Ipsa, consequatur!
+        </p>
+        <ul className={style.list}>
+          <li>Lorem ipsum dolor sit.</li>
+          <li>Explicabo, voluptate quibusdam.</li>
+          <li>Ipsa, consequatur!</li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default AboutUs;
