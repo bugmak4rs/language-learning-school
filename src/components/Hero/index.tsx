d944039bd010e2532ee815ebf34ae20003fbd6b2
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-23 14:39:47 +0300

    feat: add content in Hero.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,19 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import style from './style.module.scss';
 
 const Hero: FC = () => {
-  return <div className={style.hero}>Hero</div>;
+  return (
+    <section className={style.hero} id="hero" aria-label="Главный контент">
+      <div className={style.container}>
+        <h1 className={style.greeting}>Привет, Алексей!</h1>
+        <p className={style.progress}>
+          Ты остановился на уроке <Link to="/lesson:{id}">"Глаголы"</Link>.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
