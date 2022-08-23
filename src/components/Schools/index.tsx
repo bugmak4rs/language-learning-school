8355a7e13e18e8d66dbc55126e934b842c0e6166
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-23 14:40:16 +0300

    feat: add content in Schools.tsx

--- src/components/Schools/index.tsx
+++ src/components/Schools/index.tsx
@@ -3,7 +3,24 @@ import React, { FC } from 'react';
 import style from './style.module.scss';
 
 const Schools: FC = () => {
-  return <div className={style.schools}>Schools</div>;
+  return (
+    <section className={style.schools} id="schools" aria-label="Языковые школы">
+      <div className={style.container}>
+        <h2 className={style.title}>Языковые школы</h2>
+        <div className={style.card}>
+          <button className={style.choice}>
+            <span>Iure explicabo?</span>
+          </button>
+          <button className={style.choice}>
+            <span>Facilis repudiandae</span>
+          </button>
+          <button className={style.choice}>
+            <span>Debitis, atque</span>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Schools;
