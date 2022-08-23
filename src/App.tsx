58e971f97792651f97e7474b0317575054d91a8d
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-23 14:36:16 +0300

    feat: add main and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React, { FC } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import Header from './layout/Header';
+import Main from './layout/Main/Main';
+import Footer from './layout/Footer/Footer';
 import './scss/global.scss';
-import Header from './components/Header';
-import Home from './pages/Home';
 
 const App: FC = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Main />
+      <Footer />
     </>
   );
 };
