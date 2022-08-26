eb3eaffdc1cd1f42ac5054a57a18f38588bf134c
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-26 20:59:53 +0300

    fix: delete frame

--- src/components/EducationalVideos/index.tsx
+++ src/components/EducationalVideos/index.tsx
@@ -11,44 +11,7 @@ const EducationalVideos: FC = () => {
     >
       <div className={style.container}>
         <h2 className={style.title}>Видео</h2>
-        <div className={style.videos}>
-          <iframe
-            width="530"
-            height="285"
-            src="https://www.youtube.com/embed/cGbEUDeRtzE"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-          <iframe
-            width="530"
-            height="285"
-            src="https://www.youtube.com/embed/3GDWXTBJpfc"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-          <iframe
-            width="530"
-            height="285"
-            src="https://www.youtube.com/embed/bglVOxMaM70"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-          <iframe
-            width="530"
-            height="285"
-            src="https://www.youtube.com/embed/3im6wLx7b24"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
+        <div className={style.videos}></div>
       </div>
     </section>
   );
