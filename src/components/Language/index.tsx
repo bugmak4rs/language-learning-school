5e1ad6c2ea984d1694a695ef64646bc38d0ba7eb
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-26 19:32:21 +0300

    feat: update skelet website

--- src/components/Language/index.tsx
+++ src/components/Language/index.tsx
@@ -1,7 +1,4 @@
 import React, { FC } from 'react';
-import united from '../../assets/img/united.webp';
-import spain from '../../assets/img/spain.webp';
-import japan from '../../assets/img/japan.webp';
 
 import style from './style.module.scss';
 
@@ -12,24 +9,12 @@ const Language: FC = () => {
         <h2 className={style.title}>Все языки</h2>
         <div className={style.card}>
           <button className={style.choice}>
-            <picture>
-              <source srcSet={united} type="image/webp" />
-              <img src={'../../assets/img/united.png'} alt="flag" />
-            </picture>
             <span>Английский</span>
           </button>
           <button className={style.choice}>
-            <picture>
-              <source srcSet={spain} type="image/webp" />
-              <img src={'../../assets/img/united.png'} alt="flag" />
-            </picture>
             <span>Испанский</span>
           </button>
-          <button className={style.choice}>
-            <picture>
-              <source srcSet={japan} type="image/webp" />
-              <img src={'../../assets/img/united.png'} alt="flag" />
-            </picture>
+          <button className={style.choice} arial-label="Японский язык">
             <span>Японский</span>
           </button>
         </div>
