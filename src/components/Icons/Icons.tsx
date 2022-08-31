074a203e62cd1cdb4d7d3aa85725277e350ab874
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-31 06:16:00 +0300

    feat: add Icons.tsx for svg

---
+++ src/components/Icons/Icons.tsx
@@ -0,0 +1,19 @@
+export const Next = () => {
+  return (
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M46.5 24C46.5 36.4264 36.4264 46.5 24 46.5C11.5736 46.5 1.5 36.4264 1.5 24C1.5 11.5736 11.5736 1.5 24 1.5C36.4264 1.5 46.5 11.5736 46.5 24Z"
+        stroke="white"
+        stroke-width="3"
+        stroke-miterlimit="10"
+      />
+      <path
+        d="M22 16L30 23.5L22 31"
+        stroke="white"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
