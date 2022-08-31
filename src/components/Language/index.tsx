51c0180e97dfba2b757ce58583479163f1b14583
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-31 07:00:18 +0300

    feat: add class, images, arial-label

--- src/components/Language/index.tsx
+++ src/components/Language/index.tsx
@@ -1,4 +1,7 @@
 import React, { FC } from 'react';
+import uk from '../../assets/img/uk.png';
+import spain from '../../assets/img/spain.png';
+import japan from '../../assets/img/japan.png';
 
 import style from './style.module.scss';
 
@@ -8,14 +11,29 @@ const Language: FC = () => {
       <div className={style.container}>
         <h2 className={style.title}>Все языки</h2>
         <div className={style.card}>
-          <button className={style.choice}>
-            <span>Английский</span>
+          <button className={style.choice} arial-label="Английский язык">
+            <img src={uk} alt="Флаг Соединенных Королевств" />
+            <span className={style.choice__language}>Английский</span>
+            <div className={style.choice__info}>
+              <span className={style.choice__amount}>20</span>
+              <span className={style.choice__modules}>модулей</span>
+            </div>
           </button>
-          <button className={style.choice}>
-            <span>Испанский</span>
+          <button className={style.choice} arial-label="Испанский язык">
+            <img src={spain} alt="Флаг Испании" />
+            <span className={style.choice__language}>Испанский</span>
+            <div className={style.choice__info}>
+              <span className={style.choice__amount}>20</span>
+              <span className={style.choice__modules}>модулей</span>
+            </div>
           </button>
           <button className={style.choice} arial-label="Японский язык">
-            <span>Японский</span>
+            <img src={japan} alt="Флаг Японии" />
+            <span className={style.choice__language}>Японский</span>
+            <div className={style.choice__info}>
+              <span className={style.choice__amount}>20</span>
+              <span className={style.choice__modules}>модулей</span>
+            </div>
           </button>
         </div>
       </div>
