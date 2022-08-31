a57cbe6ac4b31a9345ae207f40c16c60c12d30db
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-01 02:15:31 +0300

    feat: add content

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,4 +1,7 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import hand from '../../assets/img/about-us-1.jpg';
+import people from '../../assets/img/about-us-2.jpg';
 
 import style from './style.module.scss';
 
@@ -7,19 +10,25 @@ const AboutUs: FC = () => {
     <section className={style.aboutUs} id="aboutUs" aria-label="О нас">
       <div className={style.container}>
         <h2 className={style.title}>О нас</h2>
-        <p className={style.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, voluptate quibusdam
-          doloremque reprehenderit, modi praesentium obcaecati rem totam aut veritatis similique,
-          debitis a dignissimos perspiciatis quam! Suscipit inventore itaque consequatur. Nesciunt
-          accusantium nulla, repellendus explicabo laudantium perspiciatis, consectetur libero,
-          mollitia consequatur expedita officia sed nostrum sit molestiae nobis. Eos ducimus nostrum
-          quaerat ea dolorem aut esse asperiores eaque. Ipsa, consequatur!
-        </p>
-        <ul className={style.list}>
-          <li>Lorem ipsum dolor sit.</li>
-          <li>Explicabo, voluptate quibusdam.</li>
-          <li>Ipsa, consequatur!</li>
-        </ul>
+        <div className={style.top}>
+          <p className={style.text}>
+            Мы&nbsp;просто энтузиасты, которые хотят помочь людям учить иностранные языки. Чтобы
+            после наших полностью бесплатных курсов человек имел базу языка которого выбрал
+            и&nbsp;дальше просто развивался и&nbsp;расширял свой вокабуляр. Нам хочется чтобы люди
+            получали удовольствие от&nbsp;изучение языков и&nbsp;развивались. Также наша программа
+            сделана так чтобы человек смог понять нравиться ему выбран язык или нет.
+          </p>
+          <img src={hand} alt="Рука с глазками" />
+        </div>
+        <div className={style.bottom}>
+          <p className={style.text}>
+            Мы&nbsp;небольшая команда <Link to={'/team'}>BUGMAKERS!</Link>, наша сфера деятельности
+            это IT, мы&nbsp;занимаемся разработкой сайтов и&nbsp;созданием веб&nbsp;&mdash;
+            приложений. И&nbsp;для нас этот продукт имеет большое знание, ведь в&nbsp;IT сейчас
+            необходим Английский и&nbsp;получить качественную базу это очень важно.
+          </p>
+          <img src={people} alt="Люди встречают закат" />
+        </div>
       </div>
     </section>
   );
