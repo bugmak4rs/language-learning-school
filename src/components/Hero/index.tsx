2d900765e780169624b79c1b3f5347388d5ef995
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-08-31 06:10:36 +0300

    fix: refactoring hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { Next } from '../Icons/Icons';
 
 import style from './style.module.scss';
 
@@ -8,9 +9,11 @@ const Hero: FC = () => {
     <section className={style.hero} id="hero" aria-label="Главный контент">
       <div className={style.container}>
         <h1 className={style.greeting}>Привет, Алексей!</h1>
-        <p className={style.progress}>
-          Ты остановился на уроке <Link to="/lesson:{id}">"Глаголы"</Link>.
-        </p>
+        <p className={style.progress}>Ты остановился на уроке</p>
+        <Link className={style.link} to="/lessons">
+          Глаголы
+          <Next />
+        </Link>
       </div>
     </section>
   );
