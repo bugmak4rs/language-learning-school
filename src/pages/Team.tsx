cb9983f49673f10523de04eb9fc3266190fd8df6
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-01 07:16:47 +0300

    feat: add page Team

---
+++ src/pages/Team.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import TeamCards from '../components/Team';
+
+const Team: FC = () => {
+  return (
+    <>
+      <TeamCards />
+    </>
+  );
+};
+
+export default Team;
