7fe930e58373d944e9a8436796950598c6c04439
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-01 05:14:48 +0300

    feat: add content Footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,11 +1,57 @@
 import React, { FC } from 'react';
-import footer from './footer.module.scss';
+import { Link } from 'react-router-dom';
+import { Logo, Telegram, Vk } from './Icon';
+
+import style from './footer.module.scss';
 
 const Footer: FC = () => {
   return (
-    <footer className={footer.offset}>
-      <div className={footer.container}>
-        <p>Footer</p>
+    <footer className={style.offset}>
+      <div className={style.container}>
+        <div className={style.top}>
+          <Link to={'/'}>
+            <Logo />
+          </Link>
+          <nav className={style.nav}>
+            <ul>
+              <li>
+                <Link to={'/team'}>BUGMAKERS!</Link>
+              </li>
+              <li>
+                <a href="#language">Языки</a>
+              </li>
+              <li>
+                <a href="#aboutUs">О нас</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href="#how">Как проходят уроки?</a>
+              </li>
+              <li>
+                <a href="#info">F.A.Q</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={style.bottom}>
+          <ul>
+            <li>
+              <a href="#" target="_blank">
+                <Telegram />
+              </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <Vk />
+              </a>
+            </li>
+          </ul>
+          <span>© 2022 BUGMAKERS!, Inc.</span>
+          <a className={style.politics} href="#" target="_blank">
+            Политика
+          </a>
+        </div>
       </div>
     </footer>
   );
